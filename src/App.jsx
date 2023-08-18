aa25682cc5179948900dd7aed9537161bae8a747
Your Name <you@example.com>
2023-08-18 23:25:00 +0200

    All Pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar'
 import { Route, Routes} from 'react-router-dom'
 import './App.css'
 import Home from './components/pages/Home'
+import Product from './components/pages/Product'
+import Services from './components/pages/Services'
+import SignUp from './components/pages/SignUp'
 
 function App() {
 
@@ -11,6 +14,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path='/' element={<Home />}/>
+          <Route exact path='/services' element={<Services />}/>
+          <Route exact path='/products' element={<Product />}/>
+          <Route exact path='/sign-up' element={<SignUp/>}/>
         </Routes>
     </>
   )
